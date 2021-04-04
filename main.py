6a9808a6f0cf07c4c5044f02a5c565786e1a9b05
olivier-be <61296476+olivier-be@users.noreply.github.com>
2021-04-04 13:13:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
+import webbrowser
 from random import randint
 from tkinter import Tk, messagebox, BOTTOM, PanedWindow, LEFT, Frame, Label, YES, Button
-import webbrowser
 
-
-window=Tk()
+window = Tk()
 window.title("démineur")
 window.geometry("558x493")
 window.minsize(568, 493)
 window.config(background='#FFFFFF')
 
 # fenetre démineur
-fenetre=PanedWindow(window)
+fenetre = PanedWindow(window)
 fenetre.pack(side=LEFT)
 fenetre.config(background='#FFFFFF')
 
-#score board
-score_board=()
+# score board
+score_board = ()
 
-#fenetre score board
+# fenetre score board
 left_window = Frame(window)
 left_window.pack(side=BOTTOM)
 left_window.config(background='#FFFFFF')
 
-#sous fentre 1
+# sous fentre 1
 def acces_credit():
     def opencredit():
         fenetre_credit.quit()
         fenetre.mainloop()
+
     fenetre_credit = Tk()
     def open_github():
-        webbrowser.open_new("https://github.com/")
+        webbrowser.open_new("https://github.com/olivier-be/d-mineurs-python")
     fenetre_credit.title("démineur")
     fenetre_credit.geometry("1080x720")
     fenetre_credit.minsize(1080, 720)
@@ -45,111 +45,118 @@ def acces_credit():
     gt_buttion.pack(side=BOTTOM, pady=100)
     quit_buttion = Button(fenetre_credit, text="quitter crédit", font=("Courrier", 20), bg='#FFFFFF',command=opencredit())
     quit_buttion.pack(side=BOTTOM, pady=50)
-button=Button(left_window,text="credit",command=acces_credit,width=20)
+button = Button(left_window, text="credit", command=acces_credit, width=20)
 button.pack(side=LEFT)
 
-#position mine
-tab_mine=[[0, 0]]
+# position mine
+tab_mine = [[0, 0]]
 
-for i in range (4):
+for i in range(4):
     tab_mine.append([randint(0, 18), randint(0, 9)])
 print(tab_mine)
 nb = 1
-tab=[Button]*200
-score=0
+tab = [Button] * 200
+score = 0
 
-#score ( case déactiver avant de toucher une mine )
+# score ( case déactiver avant de toucher une mine )
 def point():
     global score
-    score=score+1
+    score = score + 1
     print("point")
     return score
 
 def minetoucher():
     print("vous avez perdu")
     global score
-    messagebox.showerror(title="démineur", message=" tu as perdu \n partie termier !\n ton score est de {}".format(score))
-    score=str(score)
-    with open("../pythonProject3/score.py", "a+") as file:
-        file.write(score)
+    messagebox.showerror(title="démineur",message=" tu as perdu \n partie termier !\n ton score est de {}".format(score))
+    score = str(score)
+    with open("../pythonProject5/score.txt", "a+") as file:
+        file.write(score + "\n")
         print(score)
         file.close()
 
     return 0
 
-tabnb=[[0,0]]*191
-tabm=[0]*191
-i=1
-z=1
-e=1
-def verification(i,ligne,colonne):
-    e = i
+tabnb = [[0, 0]] * 191
+tabm = [0] * 191
+i = 1
+z = 1
+e = 1
+
+def verification(i, ligne, colonne):
     z = 0
-    print("start",ligne,colonne)
-    while z == 1:
+    global tab
+    print("start", ligne, colonne)
+    for e in range (2):
         u = 0
-        ligne = tabl[e][0]
-        colonne = tabl[e][1]
+        v=ligne
+        print(e)
         print("start", ligne, colonne)
-        for p in range(4):
-            if ligne + 1 != tab_mine[p][0] and colonne != tab_mine[p][1] and tab[e] != tabl[e].grid_forget :
-                tab[e].grid_forget()
-            elif ligne == tab_mine[1][0] and colonne + 1 == tab_mine[1][1] and tab[e] != tabl[e].grid_forget:
-                tab[e].grid_forget()
-            elif ligne == tab_mine[1][0] and colonne - 1 == tab_mine[1][1] and tab[e] != tabl[e].grid_forget:
-                tab[e].grid_forget()
-            elif ligne - 1 == tab_mine[1][0] and colonne == tab_mine[1][1] and tab[e] != tabl[e].grid_forget:
-                tab[e].grid_forget()
-            else:
-                u=1
-        if u==1:
+        p=0
+
+        while (u==0 or u==1 or u==2 or u==3 or u==4):
+            for b in range(191):
+                if [ligne,colonne]in tabnb:
+                    for x in range(-2,1,2):
+                        if ligne + x != tab_mine[b][0] and colonne != tab_mine[b][1] and tab[b] != tabl[b].grid_forget:
+                            print("a")
+                        elif ligne == tab_mine[b][0] and colonne + x == tab_mine[b][1] and tab[b] != tabl[b].grid_forget:
+                            tab[e+p].grid_forget()
+                            print("e")
+                        else:
+                            u += 1
+            p=p+1
+        if u == 3:
             print("vérifier")
             z = 1
-        e = e + 1
+            p+=1
+        ligne = tabnb[e + i][0]
+        colonne = tabnb[e + i][1]
 
-def change_nom_proche(i,ligne,colonne):
+def change_nom_proche(i, ligne, colonne):
     tab[i].grid_forget()
-    Label(fenetre,text="1",width=5).grid(row=ligne,column=colonne)
-    verification(i,ligne,colonne)
+    Label(fenetre, text="1", width=5).grid(row=ligne, column=colonne)
+    verification(i, ligne, colonne)
 
 # creation grille button dans une matrice
 
-def suprimer(i,ligne,colonne):
+def suprimer(i, ligne, colonne):
     tab[i].grid_forget()
-    Label(fenetre,text="0",width=5).grid(row=ligne, column=colonne)
+    Label(fenetre, text="0", width=5).grid(row=ligne, column=colonne)
     verification(i, ligne, colonne)
     point()
 
-tabl=tab
-test=0
+tabl = tab
+test = 0
 for ligne in range(19):
     for colonne in range(10):
-        if ligne == tab_mine[1][0] and colonne == tab_mine[1][1] or  ligne == tab_mine[2][0] and colonne == tab_mine[2][1] or ligne == tab_mine[3][0] and colonne == tab_mine[3][1] or ligne == tab_mine[4][0] and colonne == tab_mine[4][1] :
-            tab[i]=Button(fenetre, text="mine0", command=minetoucher)
-            tab[i].grid(row=ligne, column=colonne)
-            tabl[i]=Label(fenetre,text="2",width=5)
+        if ligne == tab_mine[1][0] and colonne == tab_mine[1][1] or ligne == tab_mine[2][0] and colonne == tab_mine[2][1] or ligne == tab_mine[3][0] and colonne == tab_mine[3][1] or ligne == tab_mine[4][0] and colonne == tab_mine[4][1]:
+            tab[i] = Button(fenetre, text="mine0", command=minetoucher).grid(row=ligne, column=colonne)
+            tabl[i] = Label(fenetre, text="2", width=5)
             tabnb[i][0] = ligne
             tabnb[i][1] = colonne
             test = 1
             print("a", i)
-        if test==0:
+        if test == 0:
             for t in range(4):
-                if((ligne == tab_mine[t + 1][0] - 1 or ligne == tab_mine[t + 1][0] + 1 or ligne == tab_mine[t + 1][0]) and (colonne == tab_mine[t + 1][1] - 1 or colonne == tab_mine[t + 1][1] + 1)) or ((ligne == tab_mine[t + 1][0] - 1 or ligne == tab_mine[t + 1][0] + 1) and (colonne == tab_mine[t + 1][1] - 1 or colonne == tab_mine[t + 1][1] + 1 or colonne == tab_mine[t + 1][1])):
-                        tab[i]=Button(fenetre, text="mine?",bg='#DC1010',width=5,command=lambda i=i,ligne=ligne,colonne=colonne: change_nom_proche(i,ligne,colonne) and point() )
-                        tab[i].grid(row=ligne, column=colonne)
-                        tabl[i]=Label(fenetre, text="1", width=5)
-                        tabnb[i][0] = ligne
-                        tabnb[i][1] = colonne
-                        test=1
-                        print("2",i)
-        if test<1:
-            tab[i] = Button(fenetre, text="mine?",width=5, command=lambda i=i,ligne=ligne,colonne=colonne: suprimer (i,ligne,colonne) and print(i))
-            tab[i].grid(row=ligne,column=colonne)
-            tabl[i]=Label(fenetre, text="0", width=5)
+                if ((ligne == tab_mine[t + 1][0] - 1 or ligne == tab_mine[t + 1][0] + 1 or ligne == tab_mine[t + 1][0]) and (colonne == tab_mine[t + 1][1] - 1 or colonne == tab_mine[t + 1][1] + 1)) or ((ligne == tab_mine[t + 1][0] - 1 or ligne == tab_mine[t + 1][0] + 1) and (colonne == tab_mine[t + 1][1] - 1 or colonne == tab_mine[t + 1][1] + 1 or colonne ==
+                        tab_mine[t + 1][1])):
+                    tab[i] = Button(fenetre, text="mine?", bg='#DC1010', width=5,
+                                    command=lambda i=i, ligne=ligne, colonne=colonne: change_nom_proche(i, ligne,colonne) and point()).grid(row=ligne, column=colonne)
+                    tabl[i] = Label(fenetre, text="1", width=5)
+                    tabnb[i][0] = ligne
+                    tabnb[i][1] = colonne
+                    test = 1
+                    print("2", i)
+        if test < 1:
+            tab[i] = Button(fenetre, text="mine?", width=5,
+                            command=lambda i=i, ligne=ligne, colonne=colonne: suprimer(i, ligne, colonne) and print(i)).grid(row=ligne, column=colonne)
+            tabl[i] = Label(fenetre, text="0", width=5)
             tabnb[i][0] = ligne
             tabnb[i][1] = colonne
             print(i)
-        i=i+1
-        test=0
+        i = i + 1
+        test = 0
 
 window.mainloop()
+
